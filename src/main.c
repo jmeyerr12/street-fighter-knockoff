1be660f1161386ce4d1f0c2c9055b79b378ab9e9
jmeyerr12 <joaomeyermuhlmann@gmail.com>
2024-05-29 22:21:53 -0300

    arrumado o handle_attack

--- src/main.c
+++ src/main.c
@@ -75,91 +75,70 @@ void draw_player(ALLEGRO_BITMAP* p, player* player, int current, int movement, i
     al_draw_bitmap_region(p, 112*current, 112*movement, 112, 112, player->x, player->y, inverted? ALLEGRO_FLIP_HORIZONTAL : 0);
 }
 
+
+void handle_player_input(ALLEGRO_KEYBOARD_STATE* key_state, player* p, const int keys[], int* movement) {
+    if (al_key_down(key_state, keys[0])) { // UP
+        if (al_key_down(key_state, keys[2])) { // RIGHT
+            joystick_up_right(p->control);
+            *movement = JUMP_FWD;
+        } else if (al_key_down(key_state, keys[1])) { // LEFT
+            joystick_up_left(p->control);
+            *movement = JUMP_BCK;
+        } else {
+            joystick_up(p->control);
+            *movement = JUMP;
+        }
+    } else if (al_key_down(key_state, keys[3])) { // DOWN
+        if (al_key_down(key_state, keys[4]) && p->attack == 0) { // PUNCH
+            *movement = DOWN_PUNCH;
+            p->attack = ATTACK_DOWN_PUNCH;
+        } else if (al_key_down(key_state, keys[5]) && p->attack == 0) { // KICK
+            *movement = DOWN_KICK;
+            p->attack = ATTACK_DOWN_KICK;
+        } else {
+            *movement = GET_DOWN;
+        }
+        p->isDown = 1;
+    } else if (al_key_down(key_state, keys[1])) { // LEFT
+        joystick_left(p->control);
+        *movement = WALK;
+    } else if (al_key_down(key_state, keys[2])) { // RIGHT
+        joystick_right(p->control);
+        *movement = WALK;
+    } else if (al_key_down(key_state, keys[4]) && p->attack == 0) { // PUNCH
+        if (p->isJumping) {
+            p->attack = ATTACK_JUMPING_PUNCH;
+            *movement = JUMPING_PUNCH;
+        } else {
+            p->attack = ATTACK_PUNCH;
+            *movement = PUNCH;
+        }
+    } else if (al_key_down(key_state, keys[5]) && p->attack == 0) { // KICK
+        if (p->isJumping) {
+            p->attack = ATTACK_JUMPING_KICK;
+            *movement = JUMPING_KICK;
+        } else if (p->isDown) {
+            p->attack = ATTACK_JUMPING_PUNCH;
+            *movement = JUMPING_PUNCH;
+        } else {
+            p->attack = ATTACK_KICK;
+            *movement = KICK;
+        }
+    } else {
+        *movement = IDLE;
+    }
+}
+
 void handle_input(ALLEGRO_KEYBOARD_STATE* key_state, player* player_1, player* player_2, int *movement1, int *movement2) {
     al_get_keyboard_state(key_state);
 
-    // Player 1 controls
-    if (al_key_down(key_state, ALLEGRO_KEY_W)) {
-        if (al_key_down(key_state, ALLEGRO_KEY_D)) {
-            joystick_up_right(player_1->control);
-            *movement1 = JUMP_BCK;
-        } else if (al_key_down(key_state, ALLEGRO_KEY_A)) {
-            joystick_up_left(player_1->control);
-            *movement1 = JUMP_FWD;
-        } else {
-            joystick_up(player_1->control);
-            *movement1 = JUMP;
-        }
-    } else if (al_key_down(key_state, DOWN_1)) {
-        if (al_key_down(key_state, ALLEGRO_KEY_R) && player_1->attack == 0) {
-            *movement1 = DOWN_PUNCH;
-            player_1->attack = ATTACK_DOWN_PUNCH;
-        } else if (al_key_down(key_state, ALLEGRO_KEY_F) && player_1->attack == 0) {
-            *movement1 = DOWN_KICK;
-            player_1->attack = ATTACK_DOWN_KICK;
-        }
-        else {
-            *movement1 = GET_DOWN;
-        }    
-        player_1->isDown = 1;
-    } else if (al_key_down(key_state, ALLEGRO_KEY_A)) {
-        joystick_left(player_1->control);
-        *movement1 = WALK;
-    } else if (al_key_down(key_state, ALLEGRO_KEY_D)) {
-        joystick_right(player_1->control);
-        *movement1 = WALK;
-    } else if (al_key_down(key_state, ALLEGRO_KEY_R) && player_1->attack == 0) {
-        if (player_1->isJumping) {
-            player_1->attack = ATTACK_JUMPING_PUNCH;
-            *movement1 = JUMPING_PUNCH;
-        } else {
-            player_1->attack = ATTACK_PUNCH;
-            *movement1 = PUNCH;
-        }
-    } else if (al_key_down(key_state, ALLEGRO_KEY_F) && player_1->attack == 0) {
-        if (player_1->isJumping) {
-            player_1->attack = ATTACK_JUMPING_KICK;
-            *movement1 = JUMPING_KICK;
-        } else if (player_1->isDown) {
-            player_1->attack = ATTACK_JUMPING_PUNCH;
-            *movement1 = JUMPING_PUNCH;            
-        } else {
-            player_1->attack = ATTACK_KICK;
-            *movement1 = KICK;
-        }
-    } else {
-        *movement1 = IDLE;
-    }
+    //definições das teclas dos jogadores
+    const int player1_keys[] = {ALLEGRO_KEY_W, ALLEGRO_KEY_A, ALLEGRO_KEY_D, ALLEGRO_KEY_S, ALLEGRO_KEY_R, ALLEGRO_KEY_F};
+    const int player2_keys[] = {ALLEGRO_KEY_UP, ALLEGRO_KEY_LEFT, ALLEGRO_KEY_RIGHT, ALLEGRO_KEY_DOWN, ALLEGRO_KEY_P, ALLEGRO_KEY_L};
 
-    // Player 2 controls
-    if (al_key_down(key_state, ALLEGRO_KEY_UP)) {
-        if (al_key_down(key_state, ALLEGRO_KEY_RIGHT)) {
-            joystick_up_right(player_2->control);
-            *movement2 = JUMP_FWD;
-        } else if (al_key_down(key_state, ALLEGRO_KEY_LEFT)) {
-            joystick_up_left(player_2->control);
-            *movement2 = JUMP_BCK;
-        } else {
-            joystick_up(player_2->control);
-            *movement2 = JUMP;
-        }
-    } else if (al_key_down(key_state, DOWN_2)) {
-        *movement2 = GET_DOWN;
-    } else if (al_key_down(key_state, ALLEGRO_KEY_LEFT)) {
-        joystick_left(player_2->control);
-        *movement2 = WALK;
-    } else if (al_key_down(key_state, ALLEGRO_KEY_RIGHT)) {
-        joystick_right(player_2->control);
-        *movement2 = WALK;
-    } else if (al_key_down(key_state, ALLEGRO_KEY_P) && player_2->attack == 0) {
-        player_2->attack = ATTACK_PUNCH;
-        *movement2 = PUNCH;
-    } else if (al_key_down(key_state, ALLEGRO_KEY_L) && player_2->attack == 0) {
-        player_2->attack = ATTACK_KICK;
-        *movement2 = KICK;
-    } else {
-        *movement2 = IDLE;																												
-    }
+    //manipulação de input dos jogadores
+    handle_player_input(key_state, player_1, player1_keys, movement1);
+    handle_player_input(key_state, player_2, player2_keys, movement2);
 }
 
 int countFrames(int movement) {
@@ -237,7 +216,7 @@ void handle_down(player *p, int mv, int *frame, int maxFrames, int timer_count) 
         if (p->attack != 0 && (*frame) >= maxFrames) p->attack = 0;
     } else {     
         if (timer_count % 10 == 0) (*frame)++;  
-        if (p->attack != 0 && (*frame) >= maxFrames) p->attack = 0;
+        if (p->attack != 0 && (*frame) >= maxFrames) p->attack = 0; //printf("ABAXAXI"); fflush(stdout);}
         if ((*frame) > maxFrames) (*frame) = 0;
         p->isDown = 0;
     }
@@ -260,41 +239,40 @@ void handle_attack(player *p, player *opponent, int *movement, int *alreadyDamag
     if (p->attack == ATTACK_KICK) {
         *movement = KICK;
         if (isInRange(p, opponent, KICK) && !(*alreadyDamaged)) {
-            (*alreadyDamaged) = 1;
-            opponent->health -= 30; //implementar sprite de sofrendo ataque!!
+            *alreadyDamaged = 1;
+            opponent->health -= 30; // implementar sprite de sofrendo ataque!!
         }
     } else if (p->attack == ATTACK_PUNCH) {
         *movement = PUNCH;
         if (isInRange(p, opponent, PUNCH) && !(*alreadyDamaged)) {
-            (*alreadyDamaged) = 1;
-            opponent->health -= 30; //implementar sprite de sofrendo ataque!!
+            *alreadyDamaged = 1;
+            opponent->health -= 30; // implementar sprite de sofrendo ataque!!
         }
-    } else if (p->attack == ATTACK_JUMPING_KICK && !(*alreadyDamaged)) {
+    } else if (p->attack == ATTACK_JUMPING_KICK) {
         *movement = JUMPING_KICK;
-        if (isInRange(p, opponent, PUNCH) && !(*alreadyDamaged)) {
-            (*alreadyDamaged) = 1;
-            opponent->health -= 30; //implementar sprite de sofrendo ataque!!
+        if (isInRange(p, opponent, JUMPING_KICK) && !(*alreadyDamaged)) {
+            *alreadyDamaged = 1;
+            opponent->health -= 30; // implementar sprite de sofrendo ataque!!
         }
-    } else if (p->attack == ATTACK_DOWN_PUNCH && !(*alreadyDamaged)) {
+    } else if (p->attack == ATTACK_DOWN_PUNCH) {
         *movement = DOWN_PUNCH;
-        if (isInRange(p, opponent, PUNCH) && !(*alreadyDamaged)) {
-            (*alreadyDamaged) = 1;
-            opponent->health -= 30; //implementar sprite de sofrendo ataque!!
+        if (isInRange(p, opponent, DOWN_PUNCH) && !(*alreadyDamaged)) {
+            *alreadyDamaged = 1;
+            opponent->health -= 30; // implementar sprite de sofrendo ataque!!
         }
-    } else if (p->attack == ATTACK_DOWN_KICK && !(*alreadyDamaged)) {
+    } else if (p->attack == ATTACK_DOWN_KICK) {
         *movement = DOWN_KICK;
-        if (isInRange(p, opponent, PUNCH) && !(*alreadyDamaged)) {
-            (*alreadyDamaged) = 1;
-            opponent->health -= 30; //implementar sprite de sofrendo ataque!!
+        if (isInRange(p, opponent, DOWN_KICK) && !(*alreadyDamaged)) {
+            *alreadyDamaged = 1;
+            opponent->health -= 30; // implementar sprite de sofrendo ataque!!
         }
-    } else if (p->attack == ATTACK_JUMPING_PUNCH && !(*alreadyDamaged)) {
+    } else if (p->attack == ATTACK_JUMPING_PUNCH) {
         *movement = JUMPING_PUNCH;
-        if (isInRange(p, opponent, PUNCH) && !(*alreadyDamaged)) {
-            (*alreadyDamaged) = 1;
-            opponent->health -= 30; //implementar sprite de sofrendo ataque!!
+        if (isInRange(p, opponent, JUMPING_PUNCH) && !(*alreadyDamaged)) {
+            *alreadyDamaged = 1;
+            opponent->health -= 30; // implementar sprite de sofrendo ataque!!
         }
-    }
-    else (*alreadyDamaged) = 0;
+    } else *alreadyDamaged = 0;
 }
 
 //retorna 0 em caso de empate, 1 em caso de vitoria do player 1 e 2 em caso de vitoria do player 2
@@ -438,6 +416,142 @@ int run_round(ALLEGRO_EVENT_QUEUE* queue, player* player_1, player* player_2, in
     return isFim;
 }
 
+//retorna 0 em caso de empate, 1 em caso de vitoria do player 1 e 2 em caso de vitoria do player 2
+/* int run_single_round(ALLEGRO_EVENT_QUEUE* queue, player* player_1, player* player_2, int* state, char* filename, 
+              ALLEGRO_FONT *font, ALLEGRO_BITMAP* player1_sheet, ALLEGRO_BITMAP* player2_sheet, int round, int p1Wins, int p2Wins) {
+
+    size** charSizes = characterSizes();
+    setDimensions(player_1, charSizes[player_1->sprite][IDLE].width, charSizes[player_1->sprite][IDLE].height);
+    setDimensions(player_2, charSizes[player_2->sprite][IDLE].width, charSizes[player_2->sprite][IDLE].height);
+    
+    ALLEGRO_TIMER* timer = al_create_timer(1.0 / 30.0);
+    al_register_event_source(queue, al_get_timer_event_source(timer));
+    al_start_timer(timer);
+
+    double lastTime = al_get_time();
+    int running = 1;
+    int frame1 = 0, frame2 = 0;
+    int timer_count = 0;
+    int maxFrame1 = 5, maxFrame2 = 5;
+    int movement1 = 0, previous_movement1 = 0;
+    int movement2 = 0, previous_movement2 = 0;
+    int alreadyDamaged1 = 0, alreadyDamaged2 = 0;
+    background bg;
+    int countdown = 99;
+    int isFim = 3; 
+    init_animated_background(&bg, 24.0, filename);  
+
+    update_animated_background(&bg);
+    al_clear_to_color(al_map_rgb(0, 0, 0));
+    draw_animated_background(&bg);
+    if (player_1->x > player_2->x){
+        draw_player(player1_sheet, player_1, frame1, movement1, 1);
+        draw_player(player2_sheet, player_2, frame2, movement2, 0);
+    } else {
+        draw_player(player1_sheet, player_1, frame1, movement1, 0);
+        draw_player(player2_sheet, player_2, frame2, movement2, 1);
+    }
+    draw_scoreboard(player_1->health,player_2->health,X_SCREEN,font,countdown,round,p1Wins,p2Wins);   
+
+    char roundText[100];
+    if (round == 1) strcpy(roundText, "1ST ROUND");
+    else if (round == 2) strcpy(roundText, "2ND ROUND");
+    else strcpy(roundText, "3RD ROUND");
+
+    al_draw_filled_rectangle(X_SCREEN/2 - 40, Y_SCREEN/2 - 4, X_SCREEN/2 + 40, Y_SCREEN/2 + 10, al_map_rgb(0, 0, 0));
+
+    al_draw_text(font, al_map_rgb(255, 255, 0),
+                 X_SCREEN / 2, Y_SCREEN / 2, ALLEGRO_ALIGN_CENTER, roundText);
+
+    al_flip_display();
+    al_rest(1.5); 
+
+    while (true) {
+        ALLEGRO_EVENT event;
+        al_wait_for_event(queue, &event);
+
+        ALLEGRO_KEYBOARD_STATE key_state;
+        al_get_keyboard_state(&key_state);
+        
+        if (event.keyboard.keycode == ALLEGRO_KEY_ESCAPE) {
+            running = !running;
+            if (running) {
+                lastTime = al_get_time(); // Reset the timer when resuming
+            }
+        }
+
+        if (running) {
+            if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+                *state = EXIT;
+                break;
+            }
+
+            isFim = verificaFim(player_1, player_2, countdown);
+            if (isFim != 3) break;
+
+            if (event.type == ALLEGRO_EVENT_TIMER) {
+                timer_count++;
+
+                double currentTime = al_get_time();
+                float deltaTime = currentTime - lastTime;
+                lastTime = currentTime;
+
+                update_animated_background(&bg);
+                al_clear_to_color(al_map_rgb(0, 0, 0));
+                draw_animated_background(&bg);
+
+                const int player1_keys[] = {ALLEGRO_KEY_W, ALLEGRO_KEY_A, ALLEGRO_KEY_D, ALLEGRO_KEY_S, ALLEGRO_KEY_R, ALLEGRO_KEY_F};
+                handle_player_input(&key_state, player_1, player1_keys, movement1);
+                
+                handle_down(player_1, movement1, &frame1, maxFrame1, timer_count);
+                handle_jump(player_1, player_2, &movement1);
+                handle_attack(player_1, player_2, &movement1, &alreadyDamaged1);
+                if (movement1 != previous_movement1) frame1 = 0;
+                maxFrame1 = countFrames(movement1);
+                previous_movement1 = movement1;
+
+                handle_down(player_2, movement2, &frame2, maxFrame2, timer_count);
+                handle_jump(player_2, player_1, &movement2);
+                handle_attack(player_2, player_1, &movement2, &alreadyDamaged2);
+                if (movement2 != previous_movement2) frame2 = 0;
+                maxFrame2 = countFrames(movement2);
+                previous_movement2 = movement2;
+
+                update_position(player_1, player_2, deltaTime);
+
+                if (player_1->x > player_2->x) {
+                    player_1->direction = RIGHT;
+                    player_2->direction = LEFT;
+                    draw_player(player1_sheet, player_1, frame1, movement1, 1);
+                    draw_player(player2_sheet, player_2, frame2, movement2, 0);
+                } else {
+                    player_1->direction = LEFT;
+                    player_2->direction = RIGHT;
+                    draw_player(player1_sheet, player_1, frame1, movement1, 0);
+                    draw_player(player2_sheet, player_2, frame2, movement2, 1);
+                }
+
+                draw_scoreboard(player_1->health, player_2->health, X_SCREEN, font, countdown, round, p1Wins, p2Wins);
+                if (timer_count % 30 == 0) countdown--;
+
+                al_flip_display();
+
+                resetPlayer(player_1);
+                resetPlayer(player_2);
+
+                updateDimensions(player_1, charSizes, movement1);
+                updateDimensions(player_2, charSizes, movement2);
+            }
+        }
+    }
+
+    
+    al_destroy_timer(timer);
+    destroy_animated_background(&bg);
+    freeCharacterSizes(charSizes);
+    return isFim;
+} */
+
 int main() {
     al_init();
     al_init_primitives_addon();
@@ -506,6 +620,28 @@ int main() {
                 state = ENDGAME;
                 break;
             case SINGLE_PLAYER:
+                /* ALLEGRO_BITMAP* player1_sheet;
+                ALLEGRO_BITMAP* player2_sheet;
+                show_characters_menu(queue,&player1_sheet,&player2_sheet,&sel1,&sel2);
+                player_1->sprite = sel1;
+                player_2->sprite = sel2;
+                selected_image = show_image_menu(font, queue);
+                if (selected_image == 0) strcpy(filename,"destroyed_dojo");
+                else if (selected_image == 1) strcpy(filename,"dark_dojo");
+                while ((p1Wins < 2) && (p2Wins < 2) && (roundCounter <= 3)) {
+                    int resultado = run_single_player(queue, player_1, player_2, &state, filename, font, player1_sheet, player2_sheet, roundCounter, p1Wins, p2Wins); 
+                    if (resultado == 0) { //em caso de empate o vencedor é sorteado
+                        resultado = 1 + rand() % 2;
+                    }
+                    if (resultado == 1) p1Wins++;
+                    else if (resultado == 2) p2Wins++;
+                    else break; //clicou para sair
+                    showWinner(font, resultado);
+                    resetAttributes(&player_1, 61, 92, 10, Y_SCREEN/2);
+                    resetAttributes(&player_2, 61, 92, X_SCREEN-122, Y_SCREEN/2);
+                    roundCounter++;
+                }
+                state = ENDGAME; */
                 break;
             case ENDGAME:
                 //mostrar endgame com vencedor e estatisticas
